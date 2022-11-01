9dfa944613a7aeac437eb9544dad180965e82e61
Divya-MT <112896433+Divya-MT@users.noreply.github.com>
2022-11-01 20:22:36 +0530

    Add files via upload

--- DB Script/DMS_Scripts.sql
+++ DB Script/DMS_Scripts.sql
@@ -3,45 +3,6 @@ GO
 USE [DeliveryManagementSystem]
 GO
 
-ALTER TABLE [dbo].[OrderDetails] DROP CONSTRAINT [FK__OrderDeta__Execu__03F0984C]
-GO
-
-ALTER TABLE [dbo].[OrderDetails] DROP CONSTRAINT [FK__OrderDeta__Custo__02FC7413]
-GO
-
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OrderDetails]') AND type in (N'U'))
-DROP TABLE [dbo].[OrderDetails]
-GO
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[OrderDetails](
-	[Order_Id] [int] IDENTITY(1,1) NOT NULL,
-	[CustomerId] [int] NOT NULL,
-	[ExecutiveId] [int] NOT NULL,
-	[DeliveryDate] [datetime] NULL,
-	[TimeOfPickup] [datetime] NULL,
-	[WeightOfPackage] [decimal](9, 3) NULL,
-	[Price] [decimal](9, 2) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Order_Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD FOREIGN KEY([CustomerId])
-REFERENCES [dbo].[Registration] ([Id])
-GO
-
-ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD FOREIGN KEY([ExecutiveId])
-REFERENCES [dbo].[Registration] ([Id])
-GO
-
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Registration]') AND type in (N'U'))
 DROP TABLE [dbo].[Registration]
 GO
@@ -67,7 +28,38 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OrderDetails]') AND type in (N'U'))
+DROP TABLE [dbo].[OrderDetails]
+GO
 
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[OrderDetails](
+	[Order_Id] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[ExecutiveId] [int] NOT NULL,
+	[DeliveryDate] [datetime] NOT NULL,
+	[TimeOfPickup] [datetime] NOT NULL,
+	[WeightOfPackage] [decimal](9, 3) NOT NULL,
+	[Price] [decimal](9, 2) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Order_Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Registration] ([Id])
+GO
+
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD FOREIGN KEY([ExecutiveId])
+REFERENCES [dbo].[Registration] ([Id])
+GO
 
 
 
--- Models/Registration.cs
+++ Models/Registration.cs
@@ -13,11 +13,12 @@ namespace DMSystemMvc.Models
         }
 
         public int Id { get; set; }
-
         public string Name { get; set; } = null!;
         public int? Age { get; set; }
+
         [DisplayName("Phone No.")]
         public string? PhoneNo { get; set; }
+
         [DisplayName("Registration Type")]
         public string? RegistrationType { get; set; }
         public string? Address { get; set; }
